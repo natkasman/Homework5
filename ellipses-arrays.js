b346879631e02b9d59cebc7a1fb19be3a5ae2e4c
Nathalia Kasman <34670412+natkasman@users.noreply.github.com>
2018-02-25 17:27:06 -0800

    Update ellipses-arrays.js

--- ellipses-arrays.js
+++ ellipses-arrays.js
@@ -4,30 +4,28 @@ var diameter = [];
 var color = [];
 
 function setup() {
-  createCanvas(400, 400);
-  colorMode(HSB)
-  
-  for (var i = 0; i < 100; i++) {
-    x[i] = random(20, 80);
-    y[i] = random(80, 100);
-    diameter[i] = random(20, 200);
-    color[i] = color(random(255), random(255), random(255));
-  }
+  createCanvas(400, 400);
+  colorMode(HSB)
+  
+  for (var i = 0; i < 100; i++) {
+    x[i] = random(180, 200);
+    y[i] = random(180, 200);
+    diameter[i] = random(360, 390);
+    color[i] = color(random(255));
+  }
 }
-  
+  
 function draw() {
-  background(255);
-  noStroke();
+  background(5);
+  noStroke();
 
-  // ellipses
-for (var i = 0; i < 100; i++) {
-	fill(color[i]);
-  ellipse(x[i], y[i], diameter[i]);
+  // ellipses
+for (var i = 0; i < 50; i++) {
+fill(color[i]);
+ellipse(x[i], y[i], diameter[i]);
+
+if (random() < 50) {
+diameter[i] = random(300, 390);
+    }
 }
-  
-  // 1% of the time
-  if (random() < 100) {
-    // random diameter between 10 and 400
-    diameter = random(100, 100);
-  }
 }
